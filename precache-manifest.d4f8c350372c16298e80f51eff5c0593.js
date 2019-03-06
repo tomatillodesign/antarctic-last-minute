2b130f6a237546bc4de1f9636b85d3c7b47f2b73
tomatillodesign <chris@tomatillodesign.com>
2019-03-06 10:27:16 -0500

    Updates

---
+++ precache-manifest.d4f8c350372c16298e80f51eff5c0593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "781a278b3a7df482097f",
+    "url": "/antarctic-last-minute/static/css/main.9fb81131.chunk.css"
+  },
+  {
+    "revision": "781a278b3a7df482097f",
+    "url": "/antarctic-last-minute/static/js/main.781a278b.chunk.js"
+  },
+  {
+    "revision": "4ad756683673bcb3be35",
+    "url": "/antarctic-last-minute/static/js/1.4ad75668.chunk.js"
+  },
+  {
+    "revision": "39100487336f7f4703e6",
+    "url": "/antarctic-last-minute/static/js/runtime~main.39100487.js"
+  },
+  {
+    "revision": "d04b00f37ea47a498359682e07a1963e",
+    "url": "/antarctic-last-minute/static/media/freestyle-whale.d04b00f3.jpg"
+  },
+  {
+    "revision": "eaa5d874765b98365d53c2f722154eb7",
+    "url": "/antarctic-last-minute/index.html"
+  }
+];
